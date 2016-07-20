32a4a282acd1cf439bb4e088b70c1a5ba1be027e
BloggerEklentileri <bloggergruplari@gmail.com>
2016-07-20 15:54:59 +0300

    Add files via upload

---
+++ srv.js
@@ -0,0 +1,11 @@
+﻿var blog = document.location.hostname;
+var slug = document.location.pathname;
+var ctld = blog.substr(blog.lastIndexOf("."));
+if (ctld != ".com") {
+  var ncr = "https://" + blog.substr(0, blog.indexOf("."));
+ncr += ".blogspot.com/ncr" + slug;
+window.location.replace(ncr);}
+//https://bloggereklentileri.blogspot.com.tr/
+//https://bloggereklentileri.blogspot.com/
+//http://cizgifilmhikayeleri.blogspot.com.tr/
+//http://cizgifilmhikayeleri.blogspot.com/
